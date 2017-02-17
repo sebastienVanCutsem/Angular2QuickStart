c9ebd657510e34e9f98b17e10c229d7b6d821361
svancutsem <sebastien.vancutsem@oniryx.be>
2017-02-17 12:30:14 +0100

    TOH multiple components and naming convention

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent }  from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+
+export class AppModule {
+
+}
